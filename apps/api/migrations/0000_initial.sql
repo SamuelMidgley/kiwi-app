7a754ee4af23246a2c2c1bbf82bda00fc9455475
Sam Midgley <midgley.sam@gmail.com>
2025-10-09 13:15:04 +0100

    Fix API and temp remove tests

--- apps/api/migrations/0000_initial.sql
+++
@@ -1,8 +0,0 @@
--- Migration number: 0000 	 2025-10-08T15:05:23.955Z
-CREATE TABLE IF NOT EXISTS WeightEntry 
-(
-    id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    weight DOUBLE NOT NULL, 
-    date_recorded VARCHAR(8000) NOT NULL
-);
--- Migration ends here
